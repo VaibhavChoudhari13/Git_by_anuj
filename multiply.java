ad65ef385cb1bc7ec138bf6019d844d70cad0706
VaibhavChoudhari13 <13vaibhavschoudhari@gmail.com>
2022-03-26 12:09:09 -0700

    multiply file commited

---
+++ multiply.java
@@ -0,0 +1,22 @@
+public class sum {
+
+
+    public static void main(String[] args)
+    {
+
+        int a = 5 ; 
+        int b = 5 ;
+
+        int c= a + b ; 
+
+        System.out.println(c);
+
+        
+        System.out.println(a+b);  // Modified 
+
+         System.out.println(a+b);  // Modified in multifly 
+
+
+
+    }
+}
